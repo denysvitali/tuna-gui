207ad75f233427df64c682612b788fcc911792ec
Denys Vitali <denys@denv.it>
2018-11-16 18:31:10 +0100

    Fix Version

--- version.hh
+++ version.hh
@@ -2,9 +2,14 @@
 
 #include <string>
 
-struct Version
-{
-    static const std::string GIT_SHA1;
-    static const std::string GIT_DATE;
-    static const std::string GIT_COMMIT_SUBJECT;
-};
+#define GUI_MAJOR 1
+#define GUI_MINOR 0
+#define GUI_PATCH 0
+
+namespace tunagui {
+    struct Version {
+        static const std::string GIT_SHA1;
+        static const std::string GIT_DATE;
+        static const std::string GIT_COMMIT_SUBJECT;
+    };
+}
