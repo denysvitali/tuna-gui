fa6a06ee6cd1260fd73e76f222ade29aaf52e4d3
Denys Vitali <denys@denv.it>
2018-11-16 18:24:46 +0100

    Add GUI Version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include "version.hh"
 #include "tuna-ge.h"
 
-int main() {
+using namespace tunagui;
+
+std::string version(){
+    std::stringstream ss{};
+    ss << "v" << GUI_MAJOR << "." << GUI_MINOR << "." << GUI_PATCH << "\t\t";
+    ss << "Commit: " << Version::GIT_SHA1 << "\tMessage: " << Version::GIT_COMMIT_SUBJECT;
+    return ss.str();
+}
+
+void doVersion(){
+    std::cout << "Tuna GUI" << std::endl;
+    std::cout << version() << std::endl;
+}
+
+int main(int argc, char** argv) {
+    for(int i=0; i<argc; i++){
+        if(strcmp(argv[i], "-v") == 0){
+            doVersion();
+            return 0;
+        }
+    }
     TunaGE::init();
+    std::cout << "Library Version: " << TunaGE::version() << std::endl;
+    std::cout << "GUI Version: " << version() << std::endl;
     return 0;
 }
