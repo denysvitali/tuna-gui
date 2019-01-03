bf0b870f9dd61caf61276321d499ca98f03152f9
Denys Vitali <denys@denv.it>
2019-01-03 17:22:27 +0100

    WIP: Fixes and some Windows-related stuff

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include <string>
 #include <sstream>
 #include <cstring>
+
+#ifdef _WINDOWS
+	#include <Windows.h>
+#endif
+
 #include "version.hh"
 #include "tuna-ge.h"
 
@@ -31,7 +36,7 @@ int main(int argc, char** argv) {
 	TunaGE::enableOriginMarker();
 	
 	TunaGE::camera.setCameraPos(glm::vec3(0.0f, 4.0f, 10.0f));  // Camera is at (0,2,10), in World Space
-	TunaGE::camera.setCameraFront(glm::vec3(0.0f, 0.0f, -1.0f)); // looks in the direction
+	TunaGE::camera.setCameraFront(glm::vec3(0.0f, -0.8f, -1.0f)); // looks in the direction
 	TunaGE::camera.setCameraUp(glm::vec3(0.0f, 1.0f, 0.0f)); // Head is up (set to 0,-1,0 to look upside-down)
 	TunaGE::camera.setCameraSpeed(0.1f);
 	TunaGE::camera.updateCamera();
@@ -50,7 +55,7 @@ int main(int argc, char** argv) {
 	mesh.setMaterial(material);
 
 	mesh.setMatrix(glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -10.0f, 0.0f)));
-	int width = 1;
+	float width = 1;
 	Vertex v1{ -width, 0, 0, 0, 1, 0, 0, 0 };
 	Vertex v2{ width, 0, 0, 0, 1, 0, 1, 1 };
 	Vertex v3{ 0, 0, -width, 0, 1, 0 , 1, 0 };
