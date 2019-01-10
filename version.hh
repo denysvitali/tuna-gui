634172ddecb1f6db775d8c0130e0ba39a762a256
Denys Vitali <denys@denv.it>
2019-01-10 15:33:00 +0100

    Fix Debug & Release properties, fix version.cc

--- version.hh
+++ version.hh
@@ -8,8 +8,8 @@
 
 namespace tunagui {
     struct Version {
-        static const std::string GIT_SHA1;
-        static const std::string GIT_DATE;
-        static const std::string GIT_COMMIT_SUBJECT;
+        static const std::string& GIT_SHA1;
+        static const std::string& GIT_DATE;
+        static const std::string& GIT_COMMIT_SUBJECT;
     };
 }
