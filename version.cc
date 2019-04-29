5ad2bb98d0dee0de3474e9b1ce80fa86c024aeef
Filippo <purafilippo@gmail.com>
2019-04-29 18:09:42 +0200

    Simplified main

--- version.cc
+++ version.cc
@@ -6,6 +6,6 @@
 #endif
 
 using namespace tunagui;
-const std::string& Version::GIT_SHA1 = "eb30ef1-dirty";
-const std::string& Version::GIT_DATE = "Wed Jan 23 10:04:34 2019";
-const std::string& Version::GIT_COMMIT_SUBJECT = "hotfix list refill every iteration";
+const std::string& Version::GIT_SHA1 = "d0d285c-dirty";
+const std::string& Version::GIT_DATE = "Mon Apr 29 18:08:09 2019";
+const std::string& Version::GIT_COMMIT_SUBJECT = "Merge branch 'master' of https://git.ded1.denv.it/shrug/tuna-gui";
