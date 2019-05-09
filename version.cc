b04b03f3c77770c96c1b35f5a494c2cd7ce941ca
Denys Vitali <denys@denv.it>
2019-05-09 18:49:37 +0200

    Fix hardcoded paths, switch to VS2019 & SDK 10.0

--- version.cc
+++ version.cc
@@ -6,6 +6,6 @@
 #endif
 
 using namespace tunagui;
-const std::string& Version::GIT_SHA1 = "59e6a92-dirty";
-const std::string& Version::GIT_DATE = "Sun May 5 15:57:31 2019";
-const std::string& Version::GIT_COMMIT_SUBJECT = "using new scene, index recognition";
+const std::string& Version::GIT_SHA1 = "fbfa03c-dirty";
+const std::string& Version::GIT_DATE = "Tue May 7 13:38:17 2019";
+const std::string& Version::GIT_COMMIT_SUBJECT = "Leapmotion implementation";
